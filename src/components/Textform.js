fe4529655644db6890b24513d5039e70bcfffac0
Sahil-0701-cell <sawantsahil041@gmail.com>
2024-11-11 16:22:19 +0530

    Changes

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -5,16 +5,19 @@ export default function Textform(props) {
         //console.log("Uppercase was clicked" + text)
         let newText=text.toLocaleUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase","success");
     }
     const handleLowClick=()=>{
       //console.log("lowercase was clicked" + text)
       let newText=text.toLowerCase();
       setText(newText);
+      props.showAlert("Converted to Lowercase","success");
     }
     const handleClearClick=()=>{
       //console.log("cleartext was clicked" + text)
       let newText='';
       setText(newText);
+      props.showAlert("Cleared Text!","success");
     }
     const handleInverseClick = () => {
       //console.log("inverse click was clicked" + text)
@@ -23,22 +26,28 @@ export default function Textform(props) {
         newtext += text[i];
       }
       setText(newtext);
+      props.showAlert("Text Inverted","success");
     }
     const handleSpeakClick = () => {
       let msg = new SpeechSynthesisUtterance();
       msg.text = text;
       window.speechSynthesis.speak(msg);
+      props.showAlert("Pronouncing your Texts","success");
     }
     const handleCapitalizeText = () => {
       let newText = text.split(" ").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
       setText(newText);
+      props.showAlert("Text Capitalized","success");
     }
     const handleCopy = () => {
       navigator.clipboard.writeText(text);
+      props.showAlert("Text Copied! ","success");
+
     }
     const handleExtraSpaces=()=>{
       let newText=text.split(/[  ]+/);
       setText(newText.join(" "));
+      props.showAlert("Removed Extra Spaces","success");
     }
     const handleOnchange=(event)=>{
         //console.log("On change")
@@ -61,21 +70,21 @@ export default function Textform(props) {
   <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'#041743'}} id="mytext" rows="10"></textarea>
   </div>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleLowClick}>Convert to Lowercase</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleClearClick}>Clear Text</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleSpeakClick}>Speak</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleInverseClick}>Inverse Text</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleCapitalizeText}>Capitalize Text</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleCopy}>Copy Text</button>
-  <button className="btn btn-primary mx-1 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleLowClick}>Convert to Lowercase</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleClearClick}>Clear Text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleSpeakClick}>Speak</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleInverseClick}>Inverse Text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleCapitalizeText}>Capitalize Text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleCopy}>Copy Text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
   </div>
     <div className="container my-3" style={{color:props.mode==='dark'?'white':'#041743'}}>
       <h2>Your text summary</h2>
       <p>{text===""? 0 : wordCount(text)} words and {text.length} characters</p>
       <p>{ text===""? 0 * 0.008 : wordCount(text) * 0.008} Minutes read</p>
       <h2>Preview</h2>
-      <p>{text.length>0?text:"Enter something int the textbox above to preview it here"}</p>
+      <p>{text.length>0?text:"Nothing to preview!!!"}</p>
     </div>
     </div>
   )
